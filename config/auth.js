ae25ed36dedbc12a90d81d41125c3dcd66b35753
hebergutierrez.p@gmail.com <hebergutierrez.p@gmail.com>
2018-02-09 21:53:34 -0600

    update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,23 +1,24 @@
+// config/auth.js
+
 // expose our config directly to our application using module.exports
 module.exports = {
 
   'facebookAuth': {
     'clientID': '154081068585463',
     'clientSecret': 'bba2e9f6859d19ef6bee3d44b0bc5477',
-    'callbackURL': 'http://localhost:8080/auth/facebook/callback',
-    'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
-    'profileFields': ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    'callbackURL': 'http://localhost:8080/auth/facebook/callback'
   },
 
   'twitterAuth': {
-    'consumerKey': '',
-    'consumerSecret': '',
+    'consumerKey': 'your-consumer-key-here',
+    'consumerSecret': 'your-client-secret-here',
     'callbackURL': 'http://localhost:8080/auth/twitter/callback'
   },
 
   'googleAuth': {
-    'clientID': '',
-    'clientSecret': '',
+    'clientID': 'your-secret-clientID-here',
+    'clientSecret': 'your-client-secret-here',
     'callbackURL': 'http://localhost:8080/auth/google/callback'
   }
+
 };
